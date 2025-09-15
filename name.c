2120b6be27ee2c283cbc1ab570c0f679fe268758
default <btmack01@louisville.edu>
2025-09-15 17:26:19 +0000

    Initial Attempt at main file

---
+++ name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "name_length.h"
+#include "reverse_name.h"
+void main () {
+    char FirstName[50]
+    char LastName[50]
+    printf("\nEnter First Name: ");
+    scanf("%s", person.FirstName);
+    printf("\nEnter Last Name: ");
+    scanf("%s", person.LastName);
+
+}
